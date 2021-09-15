e2f50a1c60e2a13b881da2b0199fc5ac770c3260
aw09 <awagung9@gmail.com>
2021-09-15 14:58:15 +0000

    add jwt auth

--- app.py
+++ app.py
@@ -3,24 +3,29 @@ from operator import is_
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, abort
 from datetime import date, datetime
 import pytz, sys
 from sqlalchemy.orm import relationship, aliased
 import os
+from flask_jwt import JWT, jwt_required, current_identity
+from hmac import compare_digest
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 db = SQLAlchemy(app=app, session_options={"autoflush": False})
 session = db.session
 ma = Marshmallow(app)
 api = Api(app)
+
 
 # ===============================================
 # Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(12), unique=True)
+    password = db.Column(db.String(50))
 
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -78,20 +83,23 @@ messages_schema = MessageSchema(many=True)
 class Register(Resource):
     def post(self):
         username = request.json['username']
+        password = request.json['password']
         checkUsername = User.query.filter_by(username=username).first()
         if checkUsername is not None:
             return ({"message": "Already exists."}, 409)     
 
         new_user = User(
-            username = username
+            username = username,
+            password = hash(password)
         )
         session.add(new_user)
         session.commit()
         return {"status": "registration completed."}
 
 class OtherUsers(Resource):
-    def get(self, user_id):
-        other_user = User.query.filter(User.id != user_id)
+    @jwt_required()
+    def get(self):
+        other_user = User.query.filter(User.id != current_identity.id)
         return users_schema.dump(other_user)
 
 class AllUsers(Resource):
@@ -100,7 +108,9 @@ class AllUsers(Resource):
         return users_schema.dump(users)
 
 class ListRoom(Resource):
-    def get(self, user_id):
+    @jwt_required()
+    def get(self):
+        user_id = current_identity.id
         user = aliased(MemberOfRoom)
         other_user = aliased(MemberOfRoom)
 
@@ -114,7 +124,9 @@ class ListRoom(Resource):
                     )
         return rooms_schema.dump(rooms)
 class RoomResource(Resource):
-    def get(self, user_id, room_id):
+    @jwt_required()
+    def get(self, room_id):
+        user_id = current_identity.id
         member = MemberOfRoom.query.filter_by(user_id = user_id, room_id = room_id).first()
         if member is None:
             return ({"message": "Invalid input."}, 422)
@@ -129,8 +141,10 @@ class RoomResource(Resource):
 
         return messages_schema.dump(messages)
         
-class SendMessage(Resource):
-    def post(self, sender_id, receiver_id):
+class MessageResource(Resource):
+    @jwt_required()
+    def post(self, receiver_id):
+        sender_id = current_identity.id
         room = checkRoom2User(sender_id, receiver_id)
 
         if room is None:
@@ -151,6 +165,18 @@ class SendMessage(Resource):
         return {"status": "message sent."}
 
 # ===============================================
+def authenticate(username, password):
+    user = User.query.filter_by(username=username).first()
+    print(user.password)
+    print(password)
+    if user and compare_digest(user.password, str(hash(password))):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return User.query.filter_by(id=user_id).first()
+
+jwt = JWT(app, authenticate, identity)
 
 def checkRoom2User(user_id_1, user_id_2):
     room_id_1 = session.query(MemberOfRoom.room_id).filter(MemberOfRoom.user_id==user_id_1)
@@ -183,13 +209,15 @@ def addMember(room_id, id_member):
     
 
 # ===============================================
+api.add_resource(AllUsers, '/')
+
 api.add_resource(Register, '/user')
-api.add_resource(OtherUsers, '/users/<int:user_id>')
-api.add_resource(AllUsers, '/users')
-api.add_resource(ListRoom, '/rooms/<int:user_id>')
-api.add_resource(RoomResource, '/room/<int:user_id>/<int:room_id>')
-api.add_resource(SendMessage, '/message/<int:sender_id>/<int:receiver_id>')
+api.add_resource(OtherUsers, '/users')
+api.add_resource(ListRoom, '/rooms')
+api.add_resource(RoomResource, '/room/<int:room_id>')
+api.add_resource(MessageResource, '/message/<int:receiver_id>')
 
 if __name__ == '__main__':
+    db.create_all()
     port = sys.argv[1] if len(sys.argv) > 1 else 2000
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
--- new.py
+++ new.py
@@ -1,15 +1,10 @@
-from app import db
-import os, requests
-import datetime
-# print(os.getcwd())
-# os.remove('./db.sqlite3')
+import requests
+
 # db.create_all()
-r = requests.post("http://localhost:2000/user", json={"username":"agung"})
+r = requests.post("http://34.101.133.218:2000/user", json={"username":"agung", "password":"1234"})
 print(r.status_code, r.reason)
-r = requests.post("http://localhost:2000/user", json={"username":"andi"})
+r = requests.post("http://34.101.133.218:2000/user", json={"username":"andi", "password":"1234"})
 print(r.status_code, r.reason)
-r = requests.post("http://localhost:2000/user", json={"username":"akbar"})
+r = requests.post("http://34.101.133.218:2000/user", json={"username":"akbar", "password":"1234"})
 print(r.status_code, r.reason)
 
-
-# print(datetime.datetime.now())
